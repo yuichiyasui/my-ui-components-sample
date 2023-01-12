7bb08144f4f160e93be74643a2a91e765eefc82b
yuichiyasui <yassan.b.a.w@gmail.com>
2023-01-13 03:50:25 +0900

    Remove unnecessary babel

--- src/components/ui/Page/Page.stories.tsx
+++ src/components/ui/Page/Page.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
@@ -9,11 +9,11 @@ export default {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
-} as ComponentMeta<typeof Page>;
+} as Meta<typeof Page>;
 
-export const LoggedOut: ComponentStoryObj<typeof Page> = {};
+export const LoggedOut: StoryObj<typeof Page> = {};
 
-export const LoggedIn: ComponentStoryObj<typeof Page> = {
+export const LoggedIn: StoryObj<typeof Page> = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const loginButton = canvas.getByRole('button', { name: /Log in/i });
